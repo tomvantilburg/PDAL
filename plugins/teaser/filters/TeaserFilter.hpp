5b7e5940e4ceda00a8f60438a66d7d5543edc42e
Howard Butler <howard@hobu.co>
2021-05-11 14:04:18 -0500

    brad suggestions

--- plugins/teaser/filters/TeaserFilter.hpp
+++ plugins/teaser/filters/TeaserFilter.hpp
@@ -40,6 +40,14 @@
 
 namespace pdal
 {
+    
+namespace std {
+template<typename T, typename... Args>
+std::unique_ptr<T> make_unique(Args&&... args)
+{
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+}
 
 class PDAL_DLL TeaserFilter : public Filter
 {
